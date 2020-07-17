68f324d3c12acf6d7404e3f740c8018cc7682b59
Ross Dobson <56548895+Ross-Dobson@users.noreply.github.com>
2020-07-17 19:58:08 +0100

    Added magit, what a life changer

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-superstar ido-grid-mode counsel cyberpunk-theme list-packages-ext yasnippet-snippets find-file-in-project elpy))))
+    (magit org-superstar ido-grid-mode counsel cyberpunk-theme list-packages-ext yasnippet-snippets find-file-in-project elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
