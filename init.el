c9b53c3aabef931729e28f4ea418507af99c738c
Ross Dobson <56548895+Ross-Dobson@users.noreply.github.com>
2020-07-18 16:58:45 +0100

    ein mostly working, about to attempt ob-ein

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  '(ein:output-area-inlined-images t)
  '(package-selected-packages
    (quote
-    (ein company-anaconda anaconda-mode company-quickhelp company paredit iedit magit org-superstar ido-grid-mode counsel cyberpunk-theme list-packages-ext yasnippet-snippets find-file-in-project elpy))))
+    (monokai-theme ein company-anaconda anaconda-mode company-quickhelp company paredit iedit magit org-superstar ido-grid-mode counsel list-packages-ext yasnippet-snippets find-file-in-project elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,17 +21,17 @@
  ;; If there is more than one, they won't work right.
  )
 
+; Theme
 (add-hook 'after-init-hook 
-	  (lambda () (load-theme 'cyberpunk t)))
+	  (lambda () (load-theme 'monokai t)))
 
+; org superstar
 (require 'org-superstar)
 (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
 (put 'narrow-to-region 'disabled nil)
 
 
 ;; cursor-chg wasn't on ELPA, so had to install manually:
-;; Tell emacs where your personal elisp lib dir is
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-
+(add-to-list 'load-path "~/.emacs.d/lisp/") ; Tell emacs where your personal elisp lib dir is
 (require 'cursor-chg)  ; Load the library
 (toggle-cursor-type-when-idle 1) ; Turn on cursor change when Emacs is idle
