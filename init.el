52d64ca9247f2d9ddd948bd78bd6b5942036c26c
Ross Dobson <56548895+Ross-Dobson@users.noreply.github.com>
2020-07-22 20:27:21 +0100

    doom-modeline installed, ghub installed

--- init.el
+++ init.el
@@ -22,11 +22,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(doom-modeline-github t)
+ '(doom-modeline-minor-modes nil)
  '(ein:output-area-inlined-images t)
  '(org-babel-load-languages (quote ((ein . t) (emacs-lisp . t))))
  '(package-selected-packages
    (quote
-    (all-the-icons-dired all-the-icons-ivy-rich all-the-icons-ivy all-the-icons monokai-theme ein company-anaconda anaconda-mode company-quickhelp company paredit iedit magit org-superstar ido-grid-mode counsel list-packages-ext yasnippet-snippets find-file-in-project elpy))))
+    (ghub doom-modeline all-the-icons-dired all-the-icons-ivy-rich all-the-icons-ivy all-the-icons monokai-theme ein company-anaconda anaconda-mode company-quickhelp company paredit iedit magit org-superstar ido-grid-mode counsel list-packages-ext yasnippet-snippets find-file-in-project elpy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
